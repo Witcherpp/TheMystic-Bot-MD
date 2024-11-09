2c99c279c167af9f8bedb80a29f63cac9e682ff6
Witcherpp <alqrnyf663@gmail.com>
2024-11-09 22:18:25 +0300

    Update 💤-reporte 2.js

--- plugins/💤-reporte 2.js
+++ plugins/💤-reporte 2.js
@@ -3,7 +3,7 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (text.length < 10) throw `*البلاغ لازم يكون فيه أكثر من ١٠ حروف!*`;
   if (text.length > 1000) throw `*الحد الأقصى للبلاغ ١٠٠٠ حرف!*`;
   const teks = `*❒ بلاغ جديد ❒*\n*≡ صاحب البلاغ :* ${m.sender.split`@`[0]}\n*≡ بلاغه:* *${text}*`;
-  conn.reply('https://chat.whatsapp.com/DzLLFkMvQEv6eFTMEyvN4f', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
+  conn.reply('120363322735352235@g.us', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
   m.reply(`> *تم ارسال البلاغ*`);
 };
 handler.help = ['reporte', 'request'].map((v) => v + ' <teks>');
