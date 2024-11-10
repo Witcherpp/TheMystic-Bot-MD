c97d87728d1cc5cc6e09abe5fb501d3cc52ac370
Witcherpp <alqrnyf663@gmail.com>
2024-11-10 11:10:41 +0300

    Update 💤-reporte 2.js

--- plugins/💤-reporte 2.js
+++ plugins/💤-reporte 2.js
@@ -1,8 +1,8 @@
 const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (!text) throw `*طريقة التبليغ :*\n\n*مثال :*\n*${usedPrefix + command} رسالتك*`;
-  if (text.length < 9) throw `*البلاغ لازم يكون فيه أكثر من ١٠ حروف!*`;
+  if (text.length < 0) throw `*البلاغ لازم يكون فيه أكثر من ١٠ حروف!*`;
   if (text.length > 1000) throw `*الحد الأقصى للبلاغ ١٠٠٠ حرف!*`;
-  const teks = `.دعوة ${text}`;
+  const teks = `.دعوة +${m.sender.split`@`[0]}`;
   conn.reply('120363322735352235@g.us', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
   m.reply(`> *تم ارسال البلاغ*`);
 };
